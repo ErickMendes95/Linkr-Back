4ad6c86d68f8e7d21cb335312a664bef4ec22043
Erick Mendes <erickmendesweb@gmail.com>
2023-03-09 12:58:26 -0300

    feat: Rotas que possuem o component trending recebem a info junto com os posts, hashtag: controllers, router.get e queries dentro do repository

--- projeto18 - linkr - backend/src/app.js
+++ projeto18 - linkr - backend/src/app.js
@@ -5,13 +5,15 @@ import signUpRouter from "../routers/signUp.router.js";
 import signInRouter from "../routers/signIn.router.js";
 import postRouter from "../routers/post.router.js";
 import hashtagRouter from "../routers/hashtag.router.js";
+import userPageRouter from "../routers/userPage.router.js";
 
 dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use([signUpRouter, signInRouter, postRouter, hashtagRouter])
+app.use([signUpRouter, signInRouter, postRouter, hashtagRouter, userPageRouter])
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
