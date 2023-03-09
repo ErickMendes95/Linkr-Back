d632c0a2cd7ae6ee4af557e1060b319cfbb55de3
luizarn <luizadriven@gmail.com>
2023-03-08 22:20:12 -0300

    refactor:post

--- projeto18 - linkr - backend/middleware/signUp.middleware.js
+++ projeto18 - linkr - backend/middleware/signUp.middleware.js
@@ -1,4 +1,16 @@
 import { db } from "../database/database.js";
 import { signUpSchema } from "../models/signUp.Schema.js";
 
-export async function postSignUpValidation(req, res, next) {}
+export async function postSignUpValidation(req, res, next) {
+  const user = req.body;
+
+  const { error } = signUpSchema.validate(user);
+
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    return res.status(422).send({ errors });
+  }
+
+  res.locals.signUp = user;
+  next()
+}
--- projeto18 - linkr - backend/src/app.js
+++ projeto18 - linkr - backend/src/app.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import signUpRouter from "../routers/signUp.router.js";
+import postRouter from "../routers/post.router.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use([signUpRouter])
+app.use([signUpRouter, postRouter])
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
