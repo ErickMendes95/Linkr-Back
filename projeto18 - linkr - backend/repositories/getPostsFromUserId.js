364655b6f7a07fa0baade79d40174b77df823764
Davi <daviribeirotesta@gmail.com>
2023-03-10 17:28:21 -0300

    feat/getUserByid

--- projeto18 - linkr - backend/repositories/getPostsFromUserId.js
+++ projeto18 - linkr - backend/repositories/getPostsFromUserId.js
@@ -5,7 +5,7 @@ export async function getPostsFromUserId(id){
     const {rows} = await db.query(`
     SELECT * 
     FROM posts 
-    WHERE id= $1
+    WHERE user_id= $1
     `, [id]
     ) 
     return rows;
