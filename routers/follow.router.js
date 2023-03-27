6731946b9c688ee939ae79b9745a8a0b015388eb
Erick Mendes <erickmendesweb@gmail.com>
2023-03-27 16:50:15 -0300

    fix/follow: retirada da autenticação

--- routers/follow.router.js
+++ routers/follow.router.js
@@ -5,8 +5,8 @@ import { authValidation } from '../middleware/auth.middleware.js';
 const followRouter = Router()
 
 followRouter.get("/user/:id/follow",getFollowStatus);
-followRouter.post("/user/:id/follow",authValidation,FollowSwitch);
-followRouter.delete("/user/:id/follow",authValidation,UnfollowSwitch);
+followRouter.post("/user/:id/follow",FollowSwitch);
+followRouter.delete("/user/:id/follow",UnfollowSwitch);
 
 
 export default followRouter;
